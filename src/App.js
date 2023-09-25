5da0eec5876388377073340460e133ba917dbb20
Christina2208 <christinalf22@gmail.com>
2023-09-25 14:33:46 -0400

    created the app.js and styles for the index.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,23 @@
 
+import Footer from "./components/footer";
+import { Route, Routes } from "react-router-dom";
+import AboutMe from "./pages/aboutMe";
+import Contact from "./pages/contact";
+import Portfolio from "./pages/portfolio";
+import Resume from "./pages/resume";
+import NavBar from "./components/navBar";
+
+export default function App() {
+  return (
+    <>
+   <NavBar />
+      <Routes>
+        <Route path="/" element={<AboutMe />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/portfolio" element={<Portfolio />} />
+        <Route path="/resume" element={<Resume />} />
+      </Routes>
+      <Footer />
+    </>
+  );
+}
